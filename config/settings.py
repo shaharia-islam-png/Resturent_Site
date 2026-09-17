fcfa2b7f7c6a45ed38d6019af73e9f3c9dc7eeee
SHAHARIA ISLAM <shaharia.cse1073@baiust.ac.bd>
2026-09-17 23:36:50 +0600

    Modify DATABASES setting for Vercel Serverless
    Updated DATABASES configuration for Vercel Serverless to use SQLite in /tmp.

--- config/settings.py
+++ config/settings.py
@@ -78,10 +78,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # ------------------------------------------------------------------
 # DATABASE
 # ------------------------------------------------------------------
+import os
+
+# DATABASES configuration for Vercel Serverless (SQLite inside /tmp)
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
 
